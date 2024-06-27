bc641542e2583dc6c1588b75cc84c0e221dd5f51
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:08:17 +0600

    Create vendor_bills.rb

---
+++ lib/doorloop/vendor_bills.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class VendorBills
+    def initialize(client)
+      @client = client
+    end
+
+    def list(options = {})
+      @client.get('vendor-bills', options)
+    end
+
+    def create(params)
+      @client.post('vendor-bills', params)
+    end
+
+    def retrieve(id)
+      @client.get("vendor-bills/#{id}")
+    end
+
+    def update(id, params)
+      @client.put("vendor-bills/#{id}", params)
+    end
+
+    def delete(id)
+      @client.delete("vendor-bills/#{id}")
+    end
+  end
+end
