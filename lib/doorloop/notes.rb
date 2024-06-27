7d8969a9dc52fb16d5b2e4c42f057653a14d4fb9
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:06:39 +0600

    add notes

---
+++ lib/doorloop/notes.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class Notes
+    def initialize(client)
+      @client = client
+    end
+
+    def list(options = {})
+      @client.get('notes', options)
+    end
+
+    def create(params)
+      @client.post('notes', params)
+    end
+
+    def retrieve(task_id)
+      @client.get("notes/#{id}")
+    end
+
+    def update(id, params)
+      @client.put("notes/#{id}", params)
+    end
+
+    def delete(id)
+      @client.delete("notes/#{id}")
+    end
+  end
+end
