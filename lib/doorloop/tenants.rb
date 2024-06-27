96b4b5f23fbe54f736c3b6e045990f7ba38c12f1
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:07:13 +0600

    add tenants

---
+++ lib/doorloop/tenants.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class Tenants
+    def initialize(client)
+      @client = client
+    end
+
+    def retrieve(id)
+      @client.get("tenants/#{id}")
+    end
+
+    def list(options = {})
+      @client.get('tenants', options)
+    end
+
+    def create(params)
+      @client.post('tenants', params)
+    end
+
+    def update(id, params)
+      @client.put("tenants/#{id}", params)
+    end
+
+    def delete(id)
+      @client.delete("tenants/#{id}")
+    end
+
+  end
+end
+
