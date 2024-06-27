3092dbfd3b2df438ca776ab960eb51509c3a9503
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:08:32 +0600

    add vendors

---
+++ lib/doorloop/vendors.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class Vendors
+    def initialize(client)
+      @client = client
+    end
+
+    def list(options = {})
+      @client.get('vendors', options)
+    end
+
+    def create(params)
+      @client.post('vendors', params)
+    end
+
+    def retrieve(id)
+      @client.get("vendors/#{id}")
+    end
+
+    def update(id, params)
+      @client.put("vendors/#{id}", params)
+    end
+
+    def delete(id)
+      @client.delete("vendors/#{id}")
+    end
+  end
+end
