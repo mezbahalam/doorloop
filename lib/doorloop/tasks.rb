4cf1ee53aa4f77844b84c44e7f0743570ceb4a72
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:07:05 +0600

    add tasks

---
+++ lib/doorloop/tasks.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class Tasks
+    def initialize(client)
+      @client = client
+    end
+
+    def list(options = {})
+      @client.get('tasks', options)
+    end
+
+    def create(params)
+      @client.post('tasks', params)
+    end
+
+    def retrieve(id)
+      @client.get("tasks/#{id}")
+    end
+
+    def update(id, params)
+      @client.put("tasks/#{id}", params)
+    end
+
+    def delete(id)
+      @client.delete("tasks/#{id}")
+    end
+
+    def post_update(params)
+      @client.post('tasks/update', params)
+    end
+  end
+end
