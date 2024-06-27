c00648ed759dab9321edbd974c8241776fb6144a
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:06:03 +0600

    add lease_charges

---
+++ lib/doorloop/lease_charges.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class LeaseCharges
+    def initialize(client)
+      @client = client
+    end
+
+    def list(options = {})
+      @client.get('lease-charges', options)
+    end
+
+    def create(params)
+      @client.post('lease-charges', params)
+    end
+
+    def retrieve(id)
+      @client.get("lease-charges/#{id}")
+    end
+
+    def update(id, params)
+      @client.put("lease-charges/#{id}", params)
+    end
+
+    def delete(id)
+      @client.delete("lease-charges/#{id}")
+    end
+  end
+end
