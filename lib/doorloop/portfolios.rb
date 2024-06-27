3f36cdfc372a91ef5e568057041c6403b08bff10
MezbahAlam <mezbahalam26@gmail.com>
2024-06-27 14:06:52 +0600

    add portfolios

---
+++ lib/doorloop/portfolios.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module DoorLoop
+  class Portfolios
+    def initialize(client)
+      @client = client
+    end
+
+    def list(options = {})
+      @client.get('property-groups', options)
+    end
+
+    def retrieve(id)
+      @client.get("property-groups/#{id}")
+    end
+  end
+end
